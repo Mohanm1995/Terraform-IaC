f6ae3d1a007c8736939e0e926188ccabfaba95db
Mohan.M <120558975+Mohanm1995@users.noreply.github.com>
2026-05-15 21:45:35 +0530

    Update AWS provider access key placeholders
    Updated access key and secret key placeholders.

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-region = "ap-south-1"
+region = "ap-south-1"                       # Update your preferred region
 access_key = "ENTER_YOUR_ACCESS_KEY"    
 secret_key = "ENTER_YOUR_ACCESS_KEY"    
 }
