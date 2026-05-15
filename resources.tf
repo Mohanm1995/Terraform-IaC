1187ebda4a59c1a1d77c5d66db433a8b4205f19c
Mohan.M <120558975+Mohanm1995@users.noreply.github.com>
2026-05-15 21:48:11 +0530

    Refactor VPC and subnet resources in Terraform

--- resources.tf
+++ resources.tf
@@ -1,113 +1,296 @@
 ### VPC
+
 resource "aws_vpc" "my_vpc" {
-  cidr_block           = "10.0.0.0/16"
-  enable_dns_support   = true
-  enable_dns_hostnames = true
+   cidr_block = "10.0.0.0/16"
+   enable_dns_support = true            
+   enable_dns_hostnames = true          
 
-  tags = {
-    Name = "my-vpc"
-  }
+   tags = {
+      Name = "my-vpc"
+   }
 }
 
-### PRIVATE SUBNET
-resource "aws_subnet" "private_subnet" {
-  vpc_id            = aws_vpc.my_vpc.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "ap-southeast-1a"
+### public subnet 1
 
-  tags = {
-    Name = "private-subnet"
-  }
+resource "aws_subnet" "public_subnet_1" {
+   vpc_id = aws_vpc.my_vpc.id
+   cidr_block = "10.0.1.0/24"
+   availability_zone = "ap-south-1a"   
+   map_public_ip_on_launch = true      
+
+   tags = {
+      Name = "public-subnet-1"
+   }
 }
 
-### PUBLIC SUBNET
-resource "aws_subnet" "public_subnet" {
-  vpc_id                  = aws_vpc.my_vpc.id
-  cidr_block              = "10.0.2.0/24"
-  availability_zone       = "ap-southeast-1a"
-  map_public_ip_on_launch = true
+### public subnet 2
 
-  tags = {
-    Name = "public-subnet"
-  }
+resource "aws_subnet" "public_subnet_2" {
+   vpc_id = aws_vpc.my_vpc.id
+   cidr_block = "10.0.2.0/24"
+   availability_zone = "ap-south-1b"   
+   map_public_ip_on_launch = true      
+
+   tags = {
+      Name = "public-subnet-2"
+   }
 }
 
-### INTERNET GATEWAY
+### private subnet 1
+
+resource "aws_subnet" "private_subnet_1" {
+   vpc_id = aws_vpc.my_vpc.id
+   cidr_block = "10.0.3.0/24"
+   availability_zone = "ap-south-1a"   
+
+   tags = {
+      Name = "private-subnet-1"
+   }
+}
+
+### private subnet 2
+
+resource "aws_subnet" "private_subnet_2" {
+   vpc_id = aws_vpc.my_vpc.id
+   cidr_block = "10.0.4.0/24"
+   availability_zone = "ap-south-1b"   
+
+   tags = {
+      Name = "private-subnet-2"
+   }
+}
+
+### private subnet 3
+
+resource "aws_subnet" "private_subnet_3" {
+   vpc_id = aws_vpc.my_vpc.id
+   cidr_block = "10.0.5.0/24"
+   availability_zone = "ap-south-1a"   
+
+   tags = {
+      Name = "private-subnet-3"
+   }
+}
+
+### private subnet 4
+
+resource "aws_subnet" "private_subnet_4" {
+   vpc_id = aws_vpc.my_vpc.id
+   cidr_block = "10.0.6.0/24"
+   availability_zone = "ap-south-1b"   
+
+   tags = {
+      Name = "private-subnet-4"
+   }
+}
+
+### private subnet 5
+
+resource "aws_subnet" "private_subnet_5" {
+   vpc_id = aws_vpc.my_vpc.id
+   cidr_block = "10.0.7.0/24"
+   availability_zone = "ap-south-1a"   
+
+   tags = {
+      Name = "private-subnet-5"
+   }
+}
+
+### private subnet 6
+
+resource "aws_subnet" "private_subnet_6" {
+   vpc_id = aws_vpc.my_vpc.id
+   cidr_block = "10.0.8.0/24"
+   availability_zone = "ap-south-1b"   
+
+   tags = {
+      Name = "private-subnet-6"
+   }
+}
+
+### internet gateway
+
 resource "aws_internet_gateway" "my_igw" {
-  vpc_id = aws_vpc.my_vpc.id
+   vpc_id = aws_vpc.my_vpc.id
 
-  tags = {
-    Name = "my-igw"
-  }
+   tags = {
+      Name = "my-igw"
+   }
 }
 
-### ROUTE TABLE
-resource "aws_route_table" "my_rt" {
-  vpc_id = aws_vpc.my_vpc.id
+### elastic ip
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.my_igw.id
-  }
+resource "aws_eip" "nat_eip" {          
+   domain = "vpc"                       
 
-  tags = {
-    Name = "my-rt"
-  }
+   tags = {
+      Name = "nat-eip"
+   }
 }
 
-### ROUTE TABLE ASSOCIATION
-resource "aws_route_table_association" "public_sub" {
-  subnet_id      = aws_subnet.public_subnet.id
-  route_table_id = aws_route_table.my_rt.id
+### nat gateway
+
+resource "aws_nat_gateway" "my_nat_gateway" {   
+   allocation_id = aws_eip.nat_eip.id           
+   subnet_id = aws_subnet.public_subnet_1.id    
+
+   tags = {
+      Name = "my-nat-gateway"
+   }
 }
 
-### SECURITY GROUP
+### public route table
+
+resource "aws_route_table" "public_rt_1" {
+   vpc_id = aws_vpc.my_vpc.id
+
+   route {
+      cidr_block = "0.0.0.0/0"
+      gateway_id = aws_internet_gateway.my_igw.id
+   }
+
+   tags = {
+      Name = "public-rt-1"
+   }
+}
+
+### private route table 1
+
+resource "aws_route_table" "private_rt_1" {
+   vpc_id = aws_vpc.my_vpc.id
+
+   route {
+      cidr_block = "0.0.0.0/0"
+      nat_gateway_id = aws_nat_gateway.my_nat_gateway.id   
+   }
+
+   tags = {
+      Name = "private-rt-1"
+   }
+}
+
+### private route table 2
+
+resource "aws_route_table" "private_rt_2" {
+   vpc_id = aws_vpc.my_vpc.id
+
+   tags = {
+      Name = "private-rt-2"
+   }
+}
+
+### private route table 3
+
+resource "aws_route_table" "private_rt_3" {
+   vpc_id = aws_vpc.my_vpc.id
+
+   route {
+      cidr_block = "0.0.0.0/0"
+      nat_gateway_id = aws_nat_gateway.my_nat_gateway.id   
+   }
+
+   tags = {
+      Name = "private-rt-3"
+   }
+}
+
+### public subnet association
+
+resource "aws_route_table_association" "public_subnet_1_association" {
+   subnet_id = aws_subnet.public_subnet_1.id
+   route_table_id = aws_route_table.public_rt_1.id
+}
+
+resource "aws_route_table_association" "public_subnet_2_association" {
+   subnet_id = aws_subnet.public_subnet_2.id
+   route_table_id = aws_route_table.public_rt_1.id
+}
+
+### private subnet association
+
+resource "aws_route_table_association" "private_subnet_1_association" {
+   subnet_id = aws_subnet.private_subnet_1.id
+   route_table_id = aws_route_table.private_rt_1.id
+}
+
+resource "aws_route_table_association" "private_subnet_2_association" {
+   subnet_id = aws_subnet.private_subnet_2.id
+   route_table_id = aws_route_table.private_rt_1.id
+}
+
+resource "aws_route_table_association" "private_subnet_3_association" {
+   subnet_id = aws_subnet.private_subnet_3.id
+   route_table_id = aws_route_table.private_rt_2.id
+}
+
+resource "aws_route_table_association" "private_subnet_4_association" {
+   subnet_id = aws_subnet.private_subnet_4.id
+   route_table_id = aws_route_table.private_rt_2.id
+}
+
+resource "aws_route_table_association" "private_subnet_5_association" {
+   subnet_id = aws_subnet.private_subnet_5.id
+   route_table_id = aws_route_table.private_rt_3.id
+}
+
+resource "aws_route_table_association" "private_subnet_6_association" {
+   subnet_id = aws_subnet.private_subnet_6.id
+   route_table_id = aws_route_table.private_rt_3.id
+}
+
+### Security group
+
 resource "aws_security_group" "nginx_sg" {
-  vpc_id = aws_vpc.my_vpc.id
+   vpc_id = aws_vpc.my_vpc.id
 
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+### inbound rule
 
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+   ingress {
+      from_port = 80
+      to_port = 80
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+   }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+   ingress {
+      from_port = 22
+      to_port = 22
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+   }
 
-  tags = {
-    Name = "nginx-sg"
-  }
+### outbound rule
+
+   egress {
+      from_port = 0
+      to_port = 0
+      protocol = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+   }
+
+   tags = {
+      Name = "nginx-sg"
+   }
 }
 
-### EC2 INSTANCE
+### EC2 instance
+
 resource "aws_instance" "nginx_server" {
-  ami                         = data.aws_ami.amazon_linux.id
-  instance_type               = "t3.micro"
-  subnet_id                   = aws_subnet.public_subnet.id
-  vpc_security_group_ids      = [aws_security_group.nginx_sg.id]
-  associate_public_ip_address = true
+   ami = "ami-009be0edec0817ffd"                                   # Change based on region
+   instance_type = "t3.micro"                                      # Modify as needed
+   subnet_id = aws_subnet.public_subnet_1.id
+   vpc_security_group_ids = [aws_security_group.nginx_sg.id]
+   associate_public_ip_address = true
 
-  user_data = <<-EOF
-              #!/bin/bash
-              sudo yum update -y
-              sudo yum install nginx -y
-              sudo systemctl start nginx
-              sudo systemctl enable nginx
-              EOF
+   user_data = <<-EOF
+               #!/bin/bash
+               yum update -y                 
+               yum install nginx -y
+               systemctl start nginx
+               systemctl enable nginx        
+               EOF
 
-  tags = {
-    Name = "nginx-server"
-  }
+   tags = {
+      Name = "nginx-server"
+   }
 }
